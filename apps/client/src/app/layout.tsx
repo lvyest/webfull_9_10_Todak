30b75be56e821c8805318f4a04b191aa3b5b9494
jhlee <jhlee89898989@gmail.com>
2026-05-22 11:53:02 +0900

    chore: next 초기 세팅
    - axios 추가
    - next 추가
    - react, react-dom 추가
    - tailwindcss 추가
    - socket.io-client 추가
    
    Ref: #2

---
+++ apps/client/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import type { ReactNode } from 'react';
+
+import { QueryProvider } from '@/providers/query-provider';
+
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Todak',
+  description: 'Todak client',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
+    </html>
+  );
+}
