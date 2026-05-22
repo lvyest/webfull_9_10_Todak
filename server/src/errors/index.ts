1f12433eab8adb6096115c3f7c20a3e2b5a475ba
jhlee <jhlee89898989@gmail.com>
2026-05-22 11:00:57 +0900

    chore: server 폴더 이동
    냉무
    
    Ref: #1

--- server/src/errors/index.ts
+++
@@ -1,6 +0,0 @@
-import { AppError } from './AppError.js';
-import { ERROR_CODES } from './error.code.js';
-import type { ErrorCodeKey } from './error.code.js';
-
-export { AppError };
-export { ERROR_CODES, ErrorCodeKey };
